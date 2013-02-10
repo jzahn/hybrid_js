ce3e31e1a5c1ec47c0c630a3bef63abc1f06ed09
Jason Zahn <smedge@gmail.com>
2013-02-10 10:55:29 -0600

    Removed two unused js files left over from last commit.

--- import.js
+++
@@ -1,9 +0,0 @@
-/*jslint vars: true, browser: true, devel: true */
-
-var importJs = function (scriptPath) {
-    "use strict";
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = scriptPath;
-    head.appendChild(script);
-};
